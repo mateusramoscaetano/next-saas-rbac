104bf677c392bbeaa9e0c2d705111da81d1e9ae6
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-05-23 13:40:45 -0300

    chore: configs

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,8 +1,4 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@rocketseat/eslint-config/react'],
-  plugins: ['simple-import-sort'],
-  rules: {
-    'simple-import-sort': 'error'
-  }
 }
