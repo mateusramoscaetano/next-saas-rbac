8927752cef1537b3eabc8cba3474fe8e1941dcbb
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-05-23 12:22:18 -0300

    config eslint prettier

---
+++ config/prettier/index.mjs
@@ -0,0 +1,20 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+
+/** @type {PrettierConfig} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false
+}
+
+export default config
